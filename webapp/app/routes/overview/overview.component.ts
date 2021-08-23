53b179e02fce796de9c49e06e6826f4a0406d2e4
鸿则 <hungtcs@outlook.com>
2021-08-23 17:37:25 +0800

    fix: gridster width error when has scrollbar

--- webapp/app/routes/overview/overview.component.ts
+++ webapp/app/routes/overview/overview.component.ts
@@ -1,6 +1,6 @@
 import * as echarts from 'echarts';
 import moment from 'moment';
-import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { CompactType, DisplayGrid, GridsterComponent, GridsterConfig, GridsterItem, GridType } from 'angular-gridster2';
 import { debounceTime, interval, Subject, switchMap, tap, timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -13,14 +13,12 @@ import { HttpClient } from '@angular/common/http';
 export class OverviewComponent implements OnInit {
   private containerResize = new Subject<void>();
 
-  @ViewChild('container', { static: true })
-  public container!: ElementRef<HTMLElement>;
-
   @ViewChild('gridster', { read: GridsterComponent, static: true })
   public gridster!: GridsterComponent;
 
   public gridsterOptions: GridsterConfig = {
     setGridSize: true,
+    disableWindowResize: true,
 
     gridType: GridType.VerticalFixed,
     displayGrid: DisplayGrid.None,
@@ -59,18 +57,12 @@ export class OverviewComponent implements OnInit {
 
     initCallback: () => {
       console.log('initCallback');
-
     },
     itemInitCallback: (item, itemComponent) => {
-      console.log('itemInitCallback', { item, itemComponent });
-
-      if(item.id === 1) {
-        this.initCharts();
-      }
-
+      // console.log('itemInitCallback', { item, itemComponent });
     },
     gridSizeChangedCallback: () => {
-      console.log('gridSizeChangedCallback');
+      // console.log('gridSizeChangedCallback', { curHeight: this.gridster.curHeight });
     },
   };
 
@@ -83,119 +75,43 @@ export class OverviewComponent implements OnInit {
     },
     {
       x: 0,
-      y: 0,
+      y: 1,
       cols: 2,
       rows: 1,
-    }
+    },
+    {
+      x: 0,
+      y: 2,
+      cols: 2,
+      rows: 1,
+    },
+    {
+      x: 0,
+      y: 3,
+      cols: 2,
+      rows: 1,
+    },
   ];
 
   constructor(
-      private readonly http: HttpClient,
-      private readonly ngZone: NgZone,
+      private readonly renderer: Renderer2,
       private readonly elementRef: ElementRef<HTMLElement>) {
 
   }
 
   ngOnInit(): void {
-    const resizeObserver = new ResizeObserver(
-      () => {
-        this.containerResize.next();
-      },
-    );
+    const resizeObserver = new ResizeObserver(() => this.containerResize.next());
     resizeObserver.observe(this.elementRef.nativeElement);
 
     this.containerResize
       // .pipe(debounceTime(128))
       .pipe(tap(() => {
         window.requestAnimationFrame(() => {
+          // keep scrollbar visible when resize grid
+          this.renderer.setStyle(this.elementRef.nativeElement, 'height', `${ this.elementRef.nativeElement.clientHeight }px`);
           this.gridster.options.api?.resize?.();
+          this.renderer.setStyle(this.elementRef.nativeElement, 'height', `auto`);
         });
-      }))
-      .subscribe();
-  }
-
-  private initCharts() {
-    const instance = echarts.init(
-      this.container.nativeElement,
-    );
-    instance.setOption({
-      title: {
-        text: '最近12小时温湿度数据'
-      },
-      tooltip: {
-        confine: true,
-      },
-      legend: {
-        data:['温度', '湿度'],
-        top: 'bottom'
-      },
-      xAxis: {},
-      yAxis: {},
-      series: [],
-    });
-    const resizeObserver = new ResizeObserver(
-      () => {
-        window.requestAnimationFrame(() => {
-          this.ngZone.runOutsideAngular(() => {
-            instance.resize()
-          });
-        });
-      },
-    );
-    resizeObserver.observe(this.container.nativeElement);
-
-    timer(0, 1000 * 20 * 2)
-      .pipe(switchMap(() => this.http.get<Array<any>>(
-        'http://192.168.10.10:1880/th',
-        {
-          params: {
-            after: moment().subtract(12, 'hours').toISOString(),
-          }
-        },
-      )))
-      .pipe(tap(data => {
-        instance.setOption({
-          tooltip: {
-            trigger: 'axis',
-            formatter: '{b}<br/>{a0}: {c0}°C<br/>{a1}: {c1}%',
-          },
-          xAxis: {
-            name: '时间',
-            type: 'category',
-            data: data.map(item => moment(item.createTime).format('HH:mm:ss')),
-          },
-          yAxis: [
-            {
-              max: 60,
-              name: '°C',
-              splitNumber: 5,
-              interval: 60 / 5,
-            },
-            {
-              name: '%',
-              max: 100,
-              min: 0,
-              splitNumber: 5,
-              interval: 20,
-            }
-          ],
-          series: [
-            {
-              type: 'line',
-              name: '温度',
-              data: data.map(item => item.temperature),
-            },
-            {
-              type: 'line',
-              name: '湿度',
-              data: data.map(item => item.humidity),
-              yAxisIndex: 1,
-            }
-          ],
-        });
-
-        console.log(instance.getOption());
-
       }))
       .subscribe();
   }
